f1d158521f65ca1c78495d394c040041fe697e0e
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-01 20:12:14 -0300

    feat: add ProductDetailsPage

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,25 @@
+import ProductInfo from "@/src/components/product/product-info";
+import ProductNotFound from "@/src/components/product/product-not-found";
+import ProductTitle from "@/src/components/product/product-title";
+import PurchaseBanner from "@/src/components/product/purchase-banner";
+import { products } from "@/src/core";
+
+export default function ProductDetailsPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = products.find((product) => product.id === +id);
+
+  return product ? (
+    <div className="flex flex-col gap-20 container py-10">
+      <div className="flex flex-col gap-10">
+        <ProductTitle product={product} />
+        <ProductInfo product={product} />
+        <PurchaseBanner product={product} />
+      </div>
+    </div>
+  ) : (
+    <ProductNotFound />
+  );
+}
