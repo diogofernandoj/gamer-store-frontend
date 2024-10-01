a51ca8cc3b43956c57fa77831d3c2d9752bf559e
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-01 20:13:10 -0300

    feat: add ProductList

---
+++ src/components/product/product-list.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { products } from "@/src/core";
+import ProductItem from "./product-item";
+import ProductNotFound from "./product-not-found";
+
+export default function ProductList() {
+  // const { produtos } = useProdutos()
+  return products.length ? (
+    <div
+      className="
+                grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5
+            "
+    >
+      {products.map((product) => (
+        <ProductItem product={product} key={product.id} />
+      ))}
+    </div>
+  ) : (
+    <ProductNotFound />
+  );
+}
