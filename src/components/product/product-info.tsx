8e7e72e59b2aa9bcd3fec5c59cf3fdcc4ab6a51e
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-01 20:13:02 -0300

    feat: add ProductInfo

---
+++ src/components/product/product-info.tsx
@@ -0,0 +1,23 @@
+import { Product } from "@/src/core";
+import Image from "next/image";
+import Specifications from "./specifications";
+
+export interface ProductInfoProps {
+  product: Product;
+}
+
+export default function ProductInfo({ product }: ProductInfoProps) {
+  return product ? (
+    <div className="flex items-center bg-violet-dark rounded-xl p-5">
+      <div className="flex-1 relative flex justify-center h-96">
+        <Image
+          src={product.image!}
+          fill
+          className="object-cover p-7"
+          alt="Imagem do Produto"
+        />
+      </div>
+      <Specifications product={product!} />
+    </div>
+  ) : null;
+}
