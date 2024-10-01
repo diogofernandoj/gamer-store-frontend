9d3c57ae1c229f9bbe497cb464cdc734261cdbfb
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-01 20:11:59 -0300

    feat: ensure layout is wrapped by Content component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Content from "../components/template/Content";
 
 const font = Montserrat({
   subsets: ["latin"],
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Content>{children}</Content>
+      </body>
     </html>
   );
 }
