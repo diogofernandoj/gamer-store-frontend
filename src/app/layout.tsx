f86268a7bfafcb2940718ebc85c2066df5f9fc1e
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-05 10:01:29 -0300

    feat: add providers to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Content from "../components/template/Content";
+import { ProductsProvider } from "../data/contexts/ProductsContext";
+import { CartProvider } from "../data/contexts/CartContext";
+import { PaymentProvider } from "../data/contexts/PaymentContext";
 
 const font = Montserrat({
   subsets: ["latin"],
@@ -20,7 +23,13 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={font.className}>
-        <Content>{children}</Content>
+        <ProductsProvider>
+          <CartProvider>
+            <PaymentProvider>
+              <Content>{children}</Content>
+            </PaymentProvider>
+          </CartProvider>
+        </ProductsProvider>
       </body>
     </html>
   );
