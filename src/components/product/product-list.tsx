16209f8226d3dac041bebc936bf60974224bed56
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-05 09:57:34 -0300

    feat: add Product context

--- src/components/product/product-list.tsx
+++ src/components/product/product-list.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { products } from "@gstore/core";
+import useProducts from "@/src/data/hooks/useProducts";
 import ProductItem from "./product-item";
 import ProductNotFound from "./product-not-found";
 
 export default function ProductList() {
-  // const { produtos } = useProdutos()
+  const { products } = useProducts();
+
   return products.length ? (
-    <div
-      className="
-                grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5
-            "
-    >
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
       {products.map((product) => (
         <ProductItem product={product} key={product.id} />
       ))}
