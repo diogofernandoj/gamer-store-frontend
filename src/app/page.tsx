43c10b69ac52183abb1283989884eef1eb559763
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-05 10:01:18 -0300

    feat: add InputSearch component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import InputSearch from "../components/product/input-search";
 import ProductList from "../components/product/product-list";
 
 export default function HomePage() {
   return (
     <div className="container">
       <div className="flex flex-wrap justify-center items-center gap-8 py-5">
+        <div className="w-full my-8">
+          <InputSearch />
+        </div>
         <ProductList />
       </div>
     </div>
